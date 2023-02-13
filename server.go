80cb0393edaacc688b0ea59d365d31a4349fde49
nffano <124452828+nffano@users.noreply.github.com>
2023-02-13 12:11:54 +0700

    01_getting_started
    - initialization
    - create entity
    - create service
    - create controller

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nffano/gin_tutorial/controller"
+	"github.com/nffano/gin_tutorial/service"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "OK!",
+		})
+	})
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+}
